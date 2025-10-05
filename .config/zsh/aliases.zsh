fe0c3ff5795f76c88b80680fd678990bf5ea20c8
ugly-custard <himanshu.mishra1123@gmail.com>
2025-10-05 19:25:11 +0530

    hypr changes

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -8,6 +8,7 @@ alias neofetch='fastfetch'
 alias calc='kalker'
 
 alias cpcode="cd ~/test/ && nvim code.cpp -c 'vsplit in' -c 'split out' -c 'split err' -c 'vertical resize -50'"
+alias hyprrebuild="paru -S --rebuild hyprcursor-git hyprgraphics-git hypridle-git hyprland-git hyprland-protocols-git hyprland-qt-support-git hyprland-qtutils-git hyprlang-git hyprlock-git hyprpaper-git hyprpicker-git hyprsunset-git hyprutils-git hyprwayland-scanner-git xdg-desktop-portal-hyprland-git aquamarine-git"
 
 # Functions
 activate() {
